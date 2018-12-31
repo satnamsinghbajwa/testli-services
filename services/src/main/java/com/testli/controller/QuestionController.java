33492bd304ffcdde6712a18323d3afa6308cafc7
manpreetkaur <manpreetkaur@192.168.0.78>
2018-12-30 23:55:20 -0700

    Get Question By Id Search Question By author and/or category Postman collection added

--- services/src/main/java/com/testli/controller/QuestionController.java
+++ services/src/main/java/com/testli/controller/QuestionController.java
@@ -1,16 +1,21 @@
 package com.testli.controller;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import static org.springframework.web.bind.annotation.RequestMethod.*;
+import static org.springframework.web.bind.annotation.RequestMethod.PUT;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.testli.data.Question;
 import com.testli.services.QuestionService;
@@ -24,6 +29,21 @@ public class QuestionController {
 	@RequestMapping(value = "/questions", method = GET, headers = "Accept=application/json")
 	public List<Question> getAllQuestions() {
 		return questionService.getAllQuestions();
+	}
+
+	@RequestMapping(value = "/questions/{id}", method = GET, headers = "Accept=application/json")
+	public Question getQuestion(@PathVariable("id") String id) {
+		Optional<Question> question = questionService.getQuestion(id);
+		if (question.isPresent()) {
+			return question.get();
+		}
+		throw new ResponseStatusException(NOT_FOUND, "Question Not Found");
+	}
+
+	@RequestMapping(value = "/questions/search", method = GET, headers = "Accept=application/json")
+	public List<Question> searchQuestion(@RequestParam(value = "author", required = false) String author,
+			@RequestParam(value = "category", required = false) String category) {
+		return questionService.searchQuestion(author, category);
 	}
 
 	@RequestMapping(value = "/questions", method = POST, headers = { "Accept=application/json" })
--- services/src/main/java/com/testli/services/QuestionService.java
+++ services/src/main/java/com/testli/services/QuestionService.java
@@ -1,8 +1,11 @@
 package com.testli.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import com.testli.data.Question;
@@ -10,10 +13,10 @@ import com.testli.data.QuestionRepository;
 
 @Service
 public class QuestionService {
-	
+
 	@Autowired
 	private QuestionRepository questionRepository;
-	
+
 	public List<Question> getAllQuestions() {
 		return questionRepository.findAll();
 	}
@@ -21,13 +24,29 @@ public class QuestionService {
 	public Question addQuestion(Question question) {
 		return questionRepository.insert(question);
 	}
-	
+
 	public Question updateQuestion(String id, Question question) {
 		question.setId(id);
-		return questionRepository.insert(question);
+		return questionRepository.save(question);
 	}
 
 	public void deleteQuestion(String id) {
 		questionRepository.deleteById(id);
+	}
+
+	public Optional<Question> getQuestion(String id) {
+		return questionRepository.findById(id);
+	}
+
+	public List<Question> searchQuestion(String author, String category) {
+		Question question = new Question();
+		question.setAuthor(author);
+		question.setCategory(category);
+
+		//ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreNullValues().withIgnoreCase();
+
+		//Example<Question> example = Example.of(question, matcher);
+		Example<Question> example = Example.of(question);
+		return questionRepository.findAll(example);
 	}
 }
