0b50232e18afaa52c0c214d8f16faedef7066605
alpeshrpatel <alpeshrpatel@gmail.com>
2019-01-03 13:34:26 -0700

    Added provider api

--- services/src/main/java/com/testli/configs/MongoConfig.java
+++ services/src/main/java/com/testli/configs/MongoConfig.java
@@ -15,14 +15,23 @@ import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
 @EnableMongoRepositories(basePackages = "com.testli.data")
 public class MongoConfig {
 	private static final String MONGO_DB_URL = "localhost";
-	private static final String MONGO_DB_NAME = "embeded_db";
+	private static final String MONGO_DB_NAME = "wis";
 
+//	@Bean
+//	public MongoTemplate mongoTemplate() throws IOException {
+//		EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
+//		mongo.setBindIp(MONGO_DB_URL);
+//		MongoClient mongoClient = mongo.getObject();
+//		MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
+//		return mongoTemplate;
+//	}
 	@Bean
-	public MongoTemplate mongoTemplate() throws IOException {
-		EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
-		mongo.setBindIp(MONGO_DB_URL);
-		MongoClient mongoClient = mongo.getObject();
-		MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
-		return mongoTemplate;
+	public MongoClient mongo() {
+		return new MongoClient("localhost");
+	}
+	
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception {
+		return new MongoTemplate(mongo(), MONGO_DB_NAME);
 	}
 }
