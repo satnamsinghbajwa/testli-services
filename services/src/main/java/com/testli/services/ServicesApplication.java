7fc54efe0da08bfb57473a6445ccad582b13a505
satnamsinghbajwa <satnam.singh.bajwa@gmail.com>
2019-01-08 23:20:07 -0700

    Feature/external mongo (#3)
    Implemented external mongodb integration

--- services/src/main/java/com/testli/services/ServicesApplication.java
+++ services/src/main/java/com/testli/services/ServicesApplication.java
@@ -3,12 +3,14 @@ package com.testli.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
 @ComponentScan(basePackages = { "com.testli"} )
+@EnableMongoRepositories(basePackages = "com.testli.data")
 public class ServicesApplication {
 
 	public static void main(String[] args) {
